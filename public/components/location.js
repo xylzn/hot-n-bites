335df207ae7c0a683b2ad4cf6b41f7748b547767
xylzn <143459592+xylzn@users.noreply.github.com>
2026-01-19 20:33:19 +0700

    Update location.js

--- public/components/location.js
+++ public/components/location.js
@@ -106,7 +106,7 @@ class HotbiteLocation extends HTMLElement {
             </div>
             <div class="map">
               <iframe
-                src="https://maps.google.com/maps?q=-6.94155,107.605594&output=embed"
+                src="https://maps.google.com/maps?q=-6.94128,107.60587&output=embed"
                 style="border:0;"
                 allowfullscreen=""
                 loading="lazy">
